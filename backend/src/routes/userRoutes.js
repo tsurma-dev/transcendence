e5ba2dc1a2fc5d2e5a157a28e362e85e98efb39d
tsurma <tsurma@student.42berlin.de>
2025-08-06 17:22:49 +0200

    Remove 2FA implementation

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -9,13 +9,16 @@ async function userRoutes(fastify) {
   fastify.get("/api/users/:username", getUser); // API change later to better serialize data
   fastify.get("/users/:username/avatar", getUserAvatar);
   fastify.get("/api/loggedinusers", listLoggedInUsers); // API wll be secured later
+
   fastify.get("/register", async (req, reply) => {
     return reply.sendFile("register.html");
   }); // will be folded into frontend
+
   fastify.get("/api/profile", {
     preHandler: fastify.verifyAuth,
     handler: profileUser,
   }); // API will be folded into frontend
+
   fastify.get("/", async (req, reply) => {
     return reply.sendFile("index.html");
   }); // will be folded into frontend
