6b17fbb2e2ad939640f87a5607ce54597a108145
tsurma <tsurma@protonmail.com>
2026-03-01 17:06:09 +0100

    Forgot to delete clutter

--- backend/src/routes/matchRoutes.js
+++
@@ -1,7 +0,0 @@
-import { handlePongWebSocket } from "../controllers/matchController.js";
-
-export default async function pongRoutes(fastify) {
-  fastify.get("/ws/pong", { websocket: true }, (socket, req) => {
-    handlePongWebSocket(socket, req);
-  });
-}
